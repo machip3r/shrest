5e82cded4989caef4f536ac3e8bd53e5c93d1fc0
MacHip3r <mac_258@hotmail.com>
2021-07-06 00:56:26 -0500

    Project Upload test

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+    //require_once "config.php";
+    //$redirectURL = "http://localhost/fb/fb-callback.php";
+    //$permissions = ["user_age_range", "user_gender", "user_photos", "user_posts"];
+    //$loginURL = $helper->getLoginUrl($redirectURL, $permissions);
+?>
+<!--<button type="button" onclick="window.location = '<?php //echo $loginURL; ?>';">Entrar con FB</button>-->
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Meta's -->
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Language" content="es">
+    <meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1">
+    <meta name="distribution" content="global">
+    <meta name="robots" content="INDEX,FOLLOW">
+    <meta name="rating" content="General">
+    <meta name="company" content="CompuBac">
+    <meta name="keywords" content="shrek, preguntas shrek, shrek questions, shrek quiz, muy muy lejano, burro, gato con botas, shrek test, tests, shrek lord, shrek games, handsome shrek, shreks kids, green ogre movie, shrek 1 prince, shrek 1 video game, shrek 4 game, shrek 2 disney, what's the dragons name in shrek">
+    <meta name="description" content="Demuestra lo que sabes y que eres un gran fanático de Shrek con este test con preguntas difíciles que ni Burro puede resolver pero sin duda el Gato con Botas resolvería con facilidad. Adelante, inténtalo.">
+    <meta property="og:title" content="Test sobre Shrek | Shrest">
+    <meta property="og:url" content="https://shrest.com/">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="assets/img/img-questions/burroshrek.jpg">
+    <meta property="og:description" content="Demuestra lo que sabes y que eres un gran fanático de Shrek con este test con preguntas difíciles que ni Burro puede resolver pero sin duda el Gato con Botas resolvería con facilidad. Adelante, inténtalo.">
+    <meta property="og:site_name" content="shrest.com">
+    <meta name="author" content="MacHip3r">
+    <meta name="designer" content="Braulio Mac">
+    <!-- Link's -->
+    <link rel="stylesheet" href="assets/css/bs-litera.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="shortcut icon" href="../assets/img/icon.png">
+    <title>Demuestra lo que sabes de Shrek | Shrest</title>
+</head>
+<body>
+    <h1 class="title-banner">Shrest | Test Shrek</h1>
+    <img class="img-logo" src="assets/img/img-index/shrest-logo.png" alt="Shrest">
+    <div class="container-banner">
+        <div class="polygon-banner">
+            <div class="form-banner"></div>
+            <div class="container-info-banner">
+                <div class="text-banner">
+                    <h4>¿Estás preparado para demostrar que eres un
+                        verdadero fan de Shrek?</h4>
+                    <br>
+                    <button class="btn btn-primary btn-lg" id="btn-index" onclick="start();">Empezar</button>
+                    <label class="subtitle">Español Latino</label>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<!-- Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="assets/js/main.js"></script>
+</html>
